cc1bdb741d40f0348b3a502bb7cc70b9c1027550
wangjipeng <zhanghui@pk4yo.com>
2018-07-03 14:08:41 +0800

    modified sha256

--- src/python/sha256.py
+++ src/python/sha256.py
@@ -3,7 +3,7 @@
 import hashlib
 
 #SHA256
-myhash = hashlib.sha256()  #sha512将是更安全的哈希算法
+myhash = hashlib.sha256()  
 myhash.update("1000001".encode("utf-8"))
 
 print(myhash.hexdigest())  #哈希密码的二进制串

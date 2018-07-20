3d3567806f329ef259bba16bbbc9f7038de19e4a
spacetiller <zhanghui@pk4yo.com>
2018-07-20 16:41:33 +0800

    add rsa key

---
+++ src/go/rsa-gen-key.go
@@ -0,0 +1,81 @@
+//
+// Crypto lib for mischn. All copyright reserved.
+// file: base58
+//
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"flag"
+	"log"
+	"os"
+)
+
+/*
+type PrivateKey struct {
+	PublicKey            // public part.
+	D         *big.Int   // private exponent
+	Primes    []*big.Int // prime factors of N, has >= 2 elements.
+
+	// Precomputed contains precomputed values that speed up private
+	// operations, if available.
+	Precomputed PrecomputedValues
+}
+
+type PublicKey struct {
+	N *big.Int // modulus
+	E int      // public exponent
+}
+*/
+func GenRsaKey(bits int) error {
+	// 生成私钥文件
+	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return err
+	}
+	derStream := x509.MarshalPKCS1PrivateKey(privateKey)
+	block := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: derStream,
+	}
+	file, err := os.Create("private.pem")
+	if err != nil {
+		return err
+	}
+	err = pem.Encode(file, block)
+	if err != nil {
+		return err
+	}
+	// 生成公钥文件
+	publicKey := &privateKey.PublicKey
+	derPkix, err := x509.MarshalPKIXPublicKey(publicKey)
+	if err != nil {
+		return err
+	}
+	block = &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: derPkix,
+	}
+	file, err = os.Create("public.pem")
+	if err != nil {
+		return err
+	}
+	err = pem.Encode(file, block)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	var bits int
+	flag.IntVar(&bits, "b", 1024, "密钥长度，默认为1024位")
+	if err := GenRsaKey(bits); err != nil {
+		log.Fatal("密钥文件生成失败！")
+	}
+	log.Println("密钥文件生成成功！")
+
+}

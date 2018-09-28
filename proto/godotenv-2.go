11a4683f70d42633cc87e2ed10b0d43bd32a7e94
spacetiller <zhanghui@pk4yo.com>
2018-09-28 19:15:54 +0800

    godotenv example 2

---
+++ proto/godotenv-2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "github.com/joho/godotenv"
+    "log"
+    "os"
+)
+
+func main() {
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+  s3Bucket := os.Getenv("S3_BUCKET")
+  secretKey := os.Getenv("SECRET_KEY")
+
+  // now do something with s3 or whatever
+}
